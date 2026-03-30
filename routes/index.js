3347b45d8cb7fec7055480d780d8c21f649a8c51
David MARTINS <david.joaquim.martins@gmail.com>
2026-03-30 03:15:57 +0200

    tableau planning 20

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { lirePlanning, listerSemaines } = require('../github');
+const { lirePlanning, listerSemaines, lireCollaborateurs } = require('../github');
 const { calcHeures, formatHeures, semaineActuelle } = require('../utils');
 
 const JOURS = ['lun', 'mar', 'mer', 'jeu', 'ven', 'sam', 'dim'];
@@ -11,14 +11,16 @@ router.get('/', async (req, res) => {
   const jourAujourdHui = JOURS_JS[new Date().getDay()];
   const semaineCourante = semaineActuelle();
   try {
-    const semaines = await listerSemaines();
+    const [semaines, collaborateurs] = await Promise.all([listerSemaines(), lireCollaborateurs()]);
     const semaineChoisie = req.query.s || semaines[0] || null;
     const { data } = semaineChoisie ? await lirePlanning(semaineChoisie) : { data: null };
 
-    // Calcul heures par équipier
+    // Calcul heures par équipier + enrichissement avec heuresHebdo
     const heuresParEmp = {};
     if (data && data.employes) {
       for (const emp of data.employes) {
+        const collab = collaborateurs.find(c => c.matricule === emp.id);
+        if (collab) emp.heuresHebdo = collab.heuresHebdo;
         heuresParEmp[emp.id || emp.nom] = {
           val: calcHeures(emp.planning),
           fmt: formatHeures(calcHeures(emp.planning))
